0cd3dec55d9a78ab90f043320f585e7003433e40
Osiris Macias <osifirsmafias99@gmail.com>
2022-11-10 20:37:17 -0600

    cvs inventory control started with header showing cvs logo

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img src="images/cvs-logo.svg" className="cvs-logo" />
+    </div>
+  );
+};
+
+export default Header;
